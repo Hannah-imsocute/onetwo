3b780902e0fd5e2f066433a79462be47e97b5815
Hannah-imsocute <hannah011221@gmail.com>
2025-08-22 14:21:08 +0900

    init: first commit for new repo

---
+++ back/backend/src/main/java/com/onetwo/backend/controller/MainController.java
@@ -0,0 +1,9 @@
+package main.java.com.onetwo.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    
+}
